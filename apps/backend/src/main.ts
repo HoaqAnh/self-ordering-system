a49e1295ac9059d693482c20b82c95d7e9b58b2e
Hoang Anh <288hoanganh@gmail.com>
2026-02-25 02:12:56 +0700

    chore: backend-setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.setGlobalPrefix('api');
-
   app.enableCors({
     origin: [
       'http://localhost:5173',
@@ -18,7 +16,33 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
-  await app.listen(process.env.PORT ?? 3000);
+  return app;
 }
-bootstrap();
+
+type VercelHandler = (req: any, res: any) => any;
+
+let cachedServer: VercelHandler;
+
+export default async function handler(req: any, res: any) {
+  if (!cachedServer) {
+    const app = await bootstrap();
+    await app.init();
+    cachedServer = app.getHttpAdapter().getInstance() as VercelHandler;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return cachedServer(req, res);
+}
+
+if (process.env.VERCEL !== '1') {
+  bootstrap()
+    .then(async (app) => {
+      await app.listen(process.env.PORT ?? 3000);
+      console.log(
+        `🚀 NestJS local server is running on http://localhost:${process.env.PORT ?? 3000}`,
+      );
+    })
+    .catch((err) => {
+      console.error('Lỗi khi khởi động server:', err);
+    });
+}
