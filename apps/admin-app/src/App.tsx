165300d033e77bace24395eac5fd52e90444e531
Hoang Anh <288hoanganh@gmail.com>
2026-02-25 00:44:19 +0700

    feat: i18next-setup

--- apps/admin-app/src/App.tsx
+++ apps/admin-app/src/App.tsx
@@ -1,8 +1,15 @@
 import { Button } from '@self-ordering/ui';
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
+   const { i18n, t } = useTranslation();
    const [loading, setLoading] = useState(false);
+
+   const toggleLanguage = () => {
+      const newLang = i18n.language === 'vi' ? 'en' : 'vi';
+      i18n.changeLanguage(newLang);
+   };
 
    const handleSubmit = () => {
       setLoading(true);
@@ -13,12 +20,18 @@ export default function App() {
    };
    return (
       <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-         <h1 className="text-2xl font-bold">Menu Khách Hàng</h1>
+         <h1 className="text-2xl font-bold">{t('dashboard.title')}</h1>
+         <div className="flex my-2 space-x-5">
+            <p>{t('common:system.changeLanguage')}</p>
+            <Button size="sm" variant="secondary" onClick={toggleLanguage}>
+               {t(`common:language.${i18n.language}`)}
+            </Button>
+         </div>
          <Button size="md" variant="primary" isLoading={loading} onClick={handleSubmit}>
-            {loading ? 'Đang lưu...' : 'Lưu dữ liệu'}
+            {loading ? t('common:system.saving') : t('common:buttons.save')}
          </Button>
          <Button size="sm" disabled variant="danger">
-            X
+            {t('dashboard.total_orders')}
          </Button>
       </div>
    );
--- apps/customer-app/src/main.tsx
+++ apps/customer-app/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './index.css';
+import '@/i18n';
 import App from './App.tsx';
 import { queryClient } from '@/lib/react-query';
 
